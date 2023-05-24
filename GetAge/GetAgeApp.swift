36677bc8022b16accc42b5c1d6063cfca022e6a3
Jevon Putra <jevon.octavian@gocement.com>
2023-05-24 17:25:20 +0700

    Initial Commit

---
+++ GetAge/GetAgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetAgeApp.swift
+//  GetAge
+//
+//  Created by Gocement on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GetAgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
