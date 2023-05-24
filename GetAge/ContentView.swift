704072418dfc5f402b8171600b5d24bd14705ae7
Jevon Putra <jevon.octavian@gocement.com>
2023-05-24 23:36:17 +0700

    add new logic

--- GetAge/ContentView.swift
+++ GetAge/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var date = Date()
+    @State private var calculateDateStr = ""
+    @State private var presentAlert = false
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            DatePicker(
+                "Select Your Birthday",
+                selection: $date,
+                displayedComponents: [.date])
+            .datePickerStyle(.wheel)
+            .labelsHidden()
+            
+            Text(calculateDateStr)
+            
+            Button{
+                let selecteDate = date
+                let today = Date()
+                
+                if(selecteDate >= today){
+                    presentAlert = true
+                } else {
+                    presentAlert = false
+                    
+                    let caledar = Calendar.current
+                    let components = caledar.dateComponents([.year, .month, .day], from: date, to: Date())
+                    
+                    calculateDateStr = "Umur \(components.day ?? 0) Hari \(components.month ?? 0) Bulan \(components.year ?? 0) Tahun"
+                }
+            } label: {
+                Text("Kalkulasi")
+                    .padding(8)
+                    .bold()
+                    .frame(maxWidth: .infinity)
+            }
+            .buttonBorderShape(.roundedRectangle)
+            .buttonStyle(.borderedProminent)
+            .tint(.orange)
         }
         .padding()
+        .alert(isPresented: $presentAlert) {
+            Alert(
+                title: Text("Error").bold(),
+                message: Text("Waktu tidak sesuai"),
+                dismissButton: .cancel(Text("Oke Mengerti"))
+            )
+        }
     }
 }
 
